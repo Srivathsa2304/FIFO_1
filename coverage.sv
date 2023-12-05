b6dbf898d8301ab6bd201e6c9cf9bc38824a123e
Srivathsa2304 <103250421+Srivathsa2304@users.noreply.github.com>
2023-12-05 15:50:22 +0530

    Update coverage.sv

--- coverage.sv
+++ coverage.sv
@@ -1,92 +1,64 @@
-class f_coverage extends uvm_subscriber #(f_sequence_item);
-  uvm_analysis_imp#(f_sequence_item, f_coverage) cov_export;
-  //----------------------------------------------------------------------------
-  `uvm_component_utils(f_coverage)
-  //----------------------------------------------------------------------------
+class fifo_subscriber extends uvm_subscriber#(f_sequence_item);
 
-  //----------------------------------------------------------------------------
-  
-  //----------------------------------------------------------------------------
+  `uvm_component_utils(fifo_subscriber)
+  uvm_analysis_imp#(f_sequence_item, fifo_subscriber) item_got_export1;
 
-  //----------------------------------------------------------------------------
-  f_sequence_item txn;
-  real in_cov, out_cov;
-  //----------------------------------------------------------------------------
+  f_sequence_item tr;
+
+covergroup cg_in;
+  option.auto_bin_max=1024;
+
+  read:coverpoint tr.i_rden;
+
+  write:coverpoint tr.i_wren;
+
+  data: coverpoint tr.i_wrdata;
+endgroup
   
-  //----------------------------------------------------------------------------
-  covergroup input_cov;
-    option.per_instance= 1;
-    option.comment     = "input_cov";
-    option.name        = "input_cov";
-    //option.auto_bin_max= 4;
-    A:coverpoint txn.i_wren { 
-        bins wren_low  ={0};
-        bins wren_high ={1};
-    }
-    B:coverpoint txn.i_rden { 
-        bins rden_low  ={0};
-        bins rden_high ={1};
-    }
-    AXB:cross A,B;//{ ignore_bins cxd = binsof(C.full_high) && binsof(D.empty_high);
-                 //}
-    
-  endgroup:input_cov;
+covergroup cg_out;
+  option.auto_bin_max=1024;
+
+  full: coverpoint tr.o_full;
+
+  alm_full: coverpoint tr.o_alm_full;
+
+  alm_empty: coverpoint tr.o_alm_empty;
+
+  empty: coverpoint tr.o_empty;
+
+  dataout : coverpoint tr.o_rddata;
+
+endgroup
+
+ 
+
+function new(string name="f_subscriber",uvm_component parent);
+
+  super.new(name,parent);
+
+  item_got_export1= new("item_got_export1", this);
+
+  tr=f_sequence_item::type_id::create("tr");
+
+  cg_in=new();
   
-  covergroup output_cov;
-    option.per_instance= 1;
-    option.comment     = "output_cov";
-    option.name        = "output_cov";
-    C:coverpoint txn.o_full { 
-        bins full_low  ={0};
-        bins full_high ={1};
-    }
-    D:coverpoint txn.o_empty { 
-        bins empty_low  ={0};
-        bins empty_high ={1};
-    }
-      CXD:cross C,D {
-        illegal_bins cxd = binsof(C.full_high) && binsof(D.empty_high);
-    }
-    E:coverpoint txn.o_alm_full { 
-        bins alm_full_low  ={0};
-        bins alm_full_high ={1};
-    }
-    F:coverpoint txn.o_alm_empty { 
-        bins alm_empty_low  ={0};
-        bins alm_empty_high ={1};
-    }
- endgroup:output_cov;
-  //----------------------------------------------------------------------------
-function new(string name="f_coverage",uvm_component parent);
-    super.new(name,parent);
-    cov_export = new("cov_export", this);
-    input_cov=new();
-    output_cov=new();
-  endfunction
-  //---------------------  write method ----------------------------------------
+  cg_out=new();
+
+endfunction
+
+ 
+
   function void write(f_sequence_item t);
-    txn=t;
-    input_cov.sample();
-    output_cov.sample();
-  endfunction
-  //----------------------------------------------------------------------------
 
-  //----------------------------------------------------------------------------
-  function void extract_phase(uvm_phase phase);
-    super.extract_phase(phase);
-    $display("HIIIIIIIII");
-    in_cov=input_cov.get_coverage();
-    out_cov=output_cov.get_coverage();
-  endfunction
-  //----------------------------------------------------------------------------
+   tr=t;
 
+   cg_in.sample();
 
-  //----------------------------------------------------------------------------
-  function void report_phase(uvm_phase phase);
-    super.report_phase(phase);
-    `uvm_info(get_type_name(),$sformatf("Input Coverage is %f \n Output Coverage is %f",in_cov, out_cov),UVM_MEDIUM)
-  endfunction
-  //----------------------------------------------------------------------------
-  
+    $display("input coverage =%0d ",cg_in.get_coverage());
+     cg_out.sample();
+
+    $display("output coverage =%0d ",cg_out.get_coverage());
+
+endfunction
+
 endclass
-            
